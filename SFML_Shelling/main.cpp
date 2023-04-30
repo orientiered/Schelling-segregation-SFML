7e5c8a91983e3975e7928bce08179ed7bdaa05de
Dmitry <da.morgachev@mail.ru>
2023-04-30 11:53:29 +0300

    Deleted multithreading, added multicolor mode and a bunch of bugs

--- SFML_Shelling/main.cpp
+++ SFML_Shelling/main.cpp
@@ -6,14 +6,21 @@
 #include <random>
 #include <SFML/Graphics/Color.hpp>
 #include <thread>
+#include <mutex>
 using namespace std;
 mt19937 mrand(time(nullptr));
-
+mutex mtx;
 struct quad {
 	uint8_t r, g, b, a;
-	quad(int r, int g, int b) : r(r), g(g), b(b), a(255) {}
+	quad(float r, float g, float b) : r(int(r)), g(int(g)), b(int(b)), a(255) {}
+	void set(sf::Uint8* colors, int i) { // ставит весь пиксель на нужное место
+		colors[i] = r;
+		colors[i+1] = g;
+		colors[i+2] = b;
+		colors[i+3] = a;
+	}
 };
-
+// hue: 0-360°; sat: 0.f-1.f; val: 0.f-1.f
 quad hsv(int hue, float sat, float val) {
 
 	hue %= 360;
@@ -32,12 +39,14 @@ quad hsv(int hue, float sat, float val) {
 	float t = val * (1.f - sat * (1 - f));
 
 	switch (h) {
-	case 6: return { (int) val * 255, (int) t * 255, (int) p * 255 };
-	case 1: return { (int) q * 255, (int) val * 255, (int) p * 255 };
-	case 2: return { (int) p * 255, (int) val * 255, (int) t * 255 };
-	case 3: return { (int) p * 255, (int) q * 255, (int) val * 255 };
-	case 4: return { (int) t * 255, (int) p * 255, (int) val * 255 };
-	case 5: return { (int) val * 255, (int) p * 255, (int) q * 255 };
+	default:
+	case 0:
+	case 6: return { val * 255, t * 255, p * 255 };
+	case 1: return { q * 255, val * 255, p * 255 };
+	case 2: return { p * 255, val * 255, t * 255 };
+	case 3: return { p * 255, q * 255, val * 255 };
+	case 4: return { t * 255, p * 255, val * 255 };
+	case 5: return { val * 255, p * 255, q * 255 };
 	}
 }
 
@@ -47,42 +56,50 @@ public:
 	unsigned n;
 	unsigned m;
 	vector<vector<int>> field;
-	float p0 = 0.2, p1 = 0.4;
+	float p0 = 0.2; //вероятность пустых клеток
+	int colors = 0; // общее количество цветов (не считая пустого)
+	bool uniform = true; // равномерное распределение цветов
+	vector<float> probs; // если неравномерное
 	const float maxr = float(2 << 16);
-	int t = 3;
+	int t = 3; // терпимость, макс. количество чужих вокруг
 	sf::Uint8* pixels;
 	sf::Texture texture;
 
-	vector<int> free;
+	vector<int> free; // массив пустых клеток
+
+	//меняем блоки из 4 Uint8 в pixels
 	void swap4(int i, int j, int k, int t) {
 		swap(pixels[4 * (i * m + j)], pixels[4 * (k * m + t)]);
-		swap(pixels[4 * (i * m + j)+1], pixels[4 * (k * m + t)+1]);
-		swap(pixels[4 * (i * m + j)+2], pixels[4 * (k * m + t)+2]);
-		swap(pixels[4 * (i * m + j)+3], pixels[4 * (k * m + t)+3]);
-	};//меняем блоки из 4 Uint8 в pixels 
-	void setup()
-	{
-		for (int i = 0; i < n; ++i)
-		{
-			for (int j = 0; j < m; ++j)
-			{
-				int rt = mrand() % int(maxr);
-				if (rt < maxr * p0)
+		swap(pixels[4 * (i * m + j) + 1], pixels[4 * (k * m + t) + 1]);
+		swap(pixels[4 * (i * m + j) + 2], pixels[4 * (k * m + t) + 2]);
+		swap(pixels[4 * (i * m + j) + 3], pixels[4 * (k * m + t) + 3]);
+	}; 
+	void setup() {
+
+		for (int i = 0; i < n; ++i) {
+			for (int j = 0; j < m; ++j) {
+
+				float rt = float(mrand() % int(maxr)) / maxr;
+
+				if (rt < p0)
 				{
 					field[i][j] = 0;
 					free.push_back(i * m + j);
 				}
-				else if (rt >= maxr * p0 && rt < maxr * (p0 + p1) ) {
-					field[i][j] = 1;
+				else if (uniform) {
+					field[i][j] = (rt-p0)/(1-p0) * colors;
 				}
 				else {
-					field[i][j] = 2;
+					int k = 0;
+					float t = p0;
+					while (t < rt) {
+						t += probs[k];
+						k++;
+					}
+					field[i][j] = k;
 				}
-				//quad c = hsv(field[i][j] * 360 / 2, 1, (field[i][j] ? 1 : 0));
-				pixels[4 * (i * m + j)] = field[i][j] == 1 ? 255 : 0 ;
-				pixels[4 * (i * m + j)+1] = field[i][j] == 2 ? 255 : 0;
-				pixels[4 * (i * m + j)+2] = 0;
-				pixels[4 * (i * m + j)+3] = 255;
+				quad col = hsv(field[i][j] * 360 / colors, 0.95 * (field[i][j] != 0), 0.95 * (field[i][j] != 0));
+				col.set(pixels, 4 * (i * m + j));
 			}
 		}
 	}
@@ -96,25 +113,21 @@ public:
 
 	int countNear(int x, int y, int r = 2)
 	{
-		int c1 = 0, c2 = 0;
-		for (int i = max(0, x - r); i < min(int(n), x + r); i++)
+		int w = m, h = n;
+		if (field[x][y] == 0) return 0;
+		vector<int> colorCount(colors+1);
+		int sum = 0;
+		for (int i = x - r; i < x + r; i++)
 		{
-			for (int j = max(0, y - r); j < min(int(m), y + r); j++)
+			for (int j = y - r; j < y + r; j++)
 			{
-				c1 += (field[i][j] == 1);
-				c2 += (field[i][j] == 2);
+				colorCount[field[(i + h) % h][(j + w) % w]]++;
+				sum += (field[(i + h) % h][(j + w) % w] > 0);
 			}
 		}
-		switch (field[x][y])
-		{
-		case 1:
-			return c2;
-		case 2:
-			return c1;
-		}
-		return 0;
+		return sum - colorCount[field[x][y] - 1];
 	}
-	
+
 	void partUpdate(int starti, int startj, int height, int width) {
 		//int perms = 0;
 		for (int i = starti; i < starti + height; ++i)
@@ -135,45 +148,41 @@ public:
 		}
 	}
 
-	int update()
+	void update()
 	{
-		int perms = 0;
-		thread t1(&Shelling::partUpdate, this, 0, 0, n / 2, m / 2);
-
-		thread t2(&Shelling::partUpdate, this, n / 2, 0, n / 2, m / 2);
-
-		thread t3(&Shelling::partUpdate, this, 0, m / 2, n / 2, m / 2);
-
-		thread t4(&Shelling::partUpdate, this, n / 2, m / 2, n / 2, m / 2);
-		t1.join();
-		t2.join();
-		t3.join();
-		t4.join();
-		return 1;
+		partUpdate(0, 0, n, m);
 	}
 
-	Shelling(unsigned n, unsigned m, float p0, float p1, float t) : n(n), m(m), p0(p0), p1(p1), t(t)
+	Shelling(unsigned n, unsigned m, float p0, int colors, int t) : n(n), m(m), p0(p0), colors(colors), t(t)
 	{
 		field.resize(n, vector<int>(m));
 		texture.create(m, n);
 		pixels = new sf::Uint8[n * m * 4];
 		setup();
 	}
-
+	Shelling(unsigned n, unsigned m, float p0, vector<float> probs, int t) : n(n), m(m), p0(p0), colors(probs.size()), probs(probs), t(t)
+	{
+		uniform = false;
+		field.resize(n, vector<int>(m));
+		texture.create(m, n);
+		pixels = new sf::Uint8[n * m * 4];
+		setup();
+	}
 	~Shelling() {
 		delete[] pixels;
 	}
 };
 
-// hue: 0-360°; sat: 0.f-1.f; val: 0.f-1.f
+
 
 
 
 int main()
 {
 	const unsigned int W = 1280;
-	const unsigned int H = 720; 
-	Shelling model(H, W, 0.1, 0.5, 4);
+	const unsigned int H = 720;
+	Shelling model(H, W, 0.01, {0.3, 0.2, 0.49}, 9);
+	//Shelling model(H, W, 0.01, {0.33, 0.32, 0.6}, 5);
 	sf::RenderWindow window(sf::VideoMode(W, H), "Shelling segregation");
 
 	while (window.isOpen())
@@ -184,7 +193,7 @@ int main()
 			if (event.type == sf::Event::Closed)
 				window.close();
 		}
-		
+
 		window.clear();
 		model.update();
 		model.draw(window);
